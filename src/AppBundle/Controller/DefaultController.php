eed41df50ce0f1225674ba174eaf3d8bcb624caa
Mihiran-Hlrm <mihiran-hlrm@localhost.localdomain>
2018-02-12 14:05:44 +0530

    Added new method to scrape links

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
+use Goutte\Client;
+
 class DefaultController extends Controller
 {
     /**
@@ -41,4 +43,66 @@ class DefaultController extends Controller
         return $data;
     }
 
+    /**
+     * @Route("/getUrls", name="getUrls" )
+     */
+    public function getUrlAction(Request $request)
+    {
+        $content = $request->getContent();
+        $content = json_decode($content,true);
+
+        $url = $content['url'];
+        $scheme = parse_url($url, PHP_URL_SCHEME);
+        $host = parse_url($url, PHP_URL_HOST);
+        $path = parse_url($url, PHP_URL_PATH);
+        $query = parse_url($url, PHP_URL_QUERY);
+
+        $cls = $content['class'];
+        $pgType = $content['pgType'];
+        $minPg = $content['minPg'];
+        $maxPg = $content['maxPg'];
+
+        if ($pgType=='p'){
+
+        }
+
+        elseif ($pgType=='q'){
+
+            $qKey = $content['qKey'];
+
+            $linksPgArr = array();
+            foreach (range($minPg,$maxPg) as $pageNum){
+
+                parse_str($query, $qs);
+                $qs[$qKey] = $pageNum;
+
+                $_url = $scheme.'://'.$host.$path.'?'.http_build_query($qs);
+
+                $linksArr = $this->getLinks($_url, $cls);
+                $linksPgArr = array_merge($linksPgArr,$linksArr);
+            }
+        }
+
+        $linksPgUArr = array_unique($linksPgArr);
+
+        $res =  new JsonResponse($linksPgUArr);
+        $res->headers->set('Content-Type', 'application/json');
+        return $res;
+    }
+
+    private function getLinks($_url, $cls){
+        $client = new Client();
+        $crawler = $client->request('GET', $_url);
+        $links = $crawler->filter('a'.$cls)->links();
+
+        $linksArr = array();
+        foreach ($links as $link) {
+            $link = $link->getUri();
+            if (parse_url($_url, PHP_URL_HOST) == parse_url($link, PHP_URL_HOST)){
+                array_push($linksArr, $link);
+            }
+        }
+
+        return $linksArr;
+    }
 }
